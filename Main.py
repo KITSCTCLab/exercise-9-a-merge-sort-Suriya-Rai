ce31741244848b40025b050803233e98d0fd849e
Suriya-Rai <100009468+Suriya-Rai@users.noreply.github.com>
2022-10-09 10:36:21 +0530

    first

--- Main.py
+++ Main.py
@@ -1,16 +1,20 @@
 from typing import List
+def merge_sort(data):
+    size = len(data)//2
+    for i in range(len(data)):
+        index = i
+        for j in range(i+1,size):
+            if data[j]<data[index]:
+                index = j
+        data[index],data[i] = data[i],data[index]
+    print(data)
 
-def merge_sort(data) -> None:
-  # Write code here
 
-
-# Do not change the following code
 input_data = input()
 data = []
 for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
+    if item.isnumeric():
+        data.append(int(item))
+    elif item.lstrip("-").isnumeric():
+        data.append(int(item))
 merge_sort(data)
-print(data)
