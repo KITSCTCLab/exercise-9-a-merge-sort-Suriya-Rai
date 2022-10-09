38d09433093aa7a3251c8389c4617e337952dec3
Suriya-Rai <100009468+Suriya-Rai@users.noreply.github.com>
2022-10-09 12:01:49 +0530

    sec

--- Main.py
+++ Main.py
@@ -1,20 +1,50 @@
+
 from typing import List
-def merge_sort(data):
-    size = len(data)//2
-    for i in range(len(data)):
-        index = i
-        for j in range(i+1,size):
-            if data[j]<data[index]:
-                index = j
-        data[index],data[i] = data[i],data[index]
-    print(data)
 
+def merge_sort(data) -> None:
+    
+    if len(data) > 1:
+        mid = len(data)//2
+        left_array = data[:mid]
+        right_array = data[mid:]
+    
+    merge_sort(left_array)
+    merge_sort(right_array)
 
+    i=0
+    j=0
+    k=0
+
+    while i<len(left_array) and j<len(right_array):
+        if (left_array[i] <= right_array[j]):
+            data[k] = left_array[i]
+            i=i+1
+        
+        else:
+            data[k] = right_array[j]
+            j=j+1
+        k=k+1
+
+    while i<len(left_array) :
+        data[k] = left_array[i]
+        i=i+1
+        k= k+1
+
+    while j<len(right_array) :
+        data[k] = right_array[j]
+        j=j+1
+        k= k+1
+    
+    
+    
+
+# Do not change the following code
 input_data = input()
 data = []
 for item in input_data.split(', '):
-    if item.isnumeric():
-        data.append(int(item))
-    elif item.lstrip("-").isnumeric():
-        data.append(int(item))
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
 merge_sort(data)
+print(data)
